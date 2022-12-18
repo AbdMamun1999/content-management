87c5fed1fe8a5cf97524760c9150ee650facebf7
Abdullah Al Mamun <mamun12121999@gmail.com>
2022-12-18 17:04:12 +0600

    create brand slider

---
+++ src/components/main/TopNews/TopNews.js
@@ -0,0 +1,74 @@
+import React from "react";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+//
+import "./TopNews.css";
+
+// import required modules
+import { Autoplay, EffectCoverflow, Pagination, Navigation } from "swiper";
+
+const TopNews = () => {
+  return (
+    <>
+      <Swiper
+        effect={"coverflow"}
+        grabCursor={true}
+        centeredSlides={true}
+        slidesPerView={3}
+        spaceBetween={30}
+        autoplay={{
+          delay: 1000,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        modules={[EffectCoverflow, Autoplay, Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-5.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-6.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-7.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-8.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-9.jpg" />
+        </SwiperSlide>
+      </Swiper>
+    </>
+  );
+};
+
+export default TopNews;
--- src/pages/main/Home.js
+++ src/pages/main/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from '../../components/main/Navbar';
+import TopNews from '../../components/main/TopNews/TopNews';
 
 const Home = () => {
     return (
         <div>
             <Navbar/>
+            <TopNews/>
         </div>
     );
 };
